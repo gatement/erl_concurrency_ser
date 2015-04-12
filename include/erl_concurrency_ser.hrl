338bf4b02a146cf163a3dc91bfd4c69435311086
Johnson Lau <gatement@gmail.com>
2015-04-12 13:15:59 +0800

    go

--- include/erl_concurrency_ser.hrl
+++ include/erl_concurrency_ser.hrl
@@ -1,1 +1,11 @@
 %-record(log, {id, at, resp_time}).
+
+%% lager
+-define(LOG_DEBUG(Msg, Args), lager:debug(Msg, Args)).
+-define(LOG_INFO(Msg, Args), lager:info(Msg, Args)).
+-define(LOG_NOTICE(Msg, Args), lager:notice(Msg, Args)).
+-define(LOG_WARNING(Msg, Args), lager:warning(Msg, Args)).
+-define(LOG_ERROR(Msg, Args), lager:error(Msg, Args)).
+-define(LOG_CRITICAL(Msg, Args), lager:critical(Msg, Args)).
+-define(LOG_ALERT(Msg, Args), lager:alert(Msg, Args)).
+-define(LOG_EMERGENCY(Msg, Args), lager:emergency(Msg, Args)).
